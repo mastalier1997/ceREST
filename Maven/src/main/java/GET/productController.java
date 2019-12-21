eb8f223e73d050ec7740946202aa4712d65dc393
mastalier1997 <jan.mastalier@gmail.con>
2019-12-21 15:02:37 +0100

    get

--- Maven/src/main/java/GET/productController.java
+++ Maven/src/main/java/GET/productController.java
@@ -10,10 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 
 public class productController {
@@ -55,7 +52,7 @@ public class productController {
     }
 
     @SuppressWarnings("Duplicates")
-    public static  void  components() throws IOException {
+    public static  void  schaltung() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
         URL urlForGetRequest = new URL("https://www.maripavi.at/produkt/schaltung?lenkertyp=Flatbarlenker");
@@ -81,12 +78,83 @@ public class productController {
 
             }
 
+           // Collection<Map.Entry<String, String>> entries = flatbarlenker.entries();
+            //entries.forEach(x-> System.out.println(x));
+        }
+
+    }
+
+    @SuppressWarnings("Duplicates")
+    public static  void  material() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        URL urlForGetRequest = new URL("https://www.maripavi.at/produkt/material?lenkertyp=Flatbarlenker");
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        conection.setRequestMethod("GET");
+        int responseCode = conection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } in .close();
+
+            String lenker =response.toString();
+            List<String> list = mapper.readValue(lenker, new TypeReference<List<String >>() {});
+
+            for (int i =0; i<list.size();i++){
+                String s ="Material";
+                String s1 =list.get(i);
+                flatbarlenker.put(s,s1);
+
+            }
+
+           // Collection<Map.Entry<String, String>> entries = flatbarlenker.entries();
+            //entries.forEach(x-> System.out.println(x));
+        }
+
+    }
+
+    @SuppressWarnings("Duplicates")
+    public static  void  griff() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        URL urlForGetRequest = new URL("https://www.maripavi.at/produkt/schaltung?lenkertyp=Flatbarlenker");
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        conection.setRequestMethod("GET");
+        int responseCode = conection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } in .close();
+
+            String lenker =response.toString();
+            List<String> list = mapper.readValue(lenker, new TypeReference<List<String >>() {});
+
+            for (int i =0; i<list.size();i++){
+                String s ="Schaltung";
+                String s1 =list.get(i);
+                flatbarlenker.put(s,s1);
+
+            }
+
+            Collection<Map.Entry<String, String>> entries = flatbarlenker.entries();
+            entries.forEach(x-> System.out.println(x));
         }
 
     }
 
     public static void main(String[] args) throws IOException {
         //lenkertyp();
-        components();
+        schaltung();
+        material();
+        Collection<Map.Entry<String, String>> entries = flatbarlenker.entries();
+        entries.forEach(x-> System.out.println(x));
     }
 }
