c6648f85ec8ac4f2a59e3ad1a9c89ef969adcda8
mastalier1997 <jan.mastalier@gmail.con>
2019-12-24 15:02:36 +0100

    application

--- Maven/src/main/java/POST/POSTApplication.java
+++ Maven/src/main/java/POST/POSTApplication.java
@@ -2,7 +2,6 @@ package POST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 @SpringBootApplication
 public class POSTApplication {
